dbddb63ae1e070b25dcd997a33ef2d35f135c347
Yagoxdll <113265293+Yagoxdll@users.noreply.github.com>
2024-04-14 23:50:57 -0300

    Update queues.c
    In this code refactoring, several improvements were made to enhance clarity and adherence to standard naming conventions. The original structure st_queue was renamed to Queue, aligning with common English naming conventions. Similarly, function names such as enqueue, dequeue, imprimir, and re_start_queue were updated to enqueue, dequeue, printQueue, and clearQueue, respectively, to maintain consistency and improve readability. Additionally, the logic of the dequeue function was corrected to ensure proper removal of elements from the queue, addressing a bug where the first value was incorrectly retained after a dequeue operation. These changes contribute to a more comprehensible and maintainable codebase

--- queues.c
+++ queues.c
@@ -1,49 +1,57 @@
-//Basic use of queues using C.
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void allocate(int **queue, int *size) {
-    (*size)++;
-    *queue = (int*)realloc(*queue, (*size) * sizeof(int));
+struct Queue {
+    int *values;
+    int head;
+    int tail;
+};
 
-    if (*queue == NULL) {
-        printf("Memory allocation error\n");
-        exit(EXIT_FAILURE);
+void enqueue(int value, struct Queue *queue) {
+    queue->tail++;
+    queue->values = realloc(queue->values, queue->tail * sizeof(int));
+    queue->values[queue->tail - 1] = value;
+}
+
+void dequeue(struct Queue *queue) {
+    if (queue->tail > 0) {
+        for (int i = 0; i < queue->tail - 1; i++) {
+            queue->values[i] = queue->values[i + 1];
+        }
+        queue->tail--;
+        queue->values = realloc(queue->values, queue->tail * sizeof(int));
     }
 }
 
-void enqueue(int value, int **queue, int *size) {
-    allocate(queue, size);
-    (*queue)[*size - 1] = value;
+void printQueue(struct Queue *queue) {
+    printf("Queue: ");
+    for (int i = 0; i < queue->tail; i++) {
+        printf("%d -> ", queue->values[i]);
+    }
+    printf("NULL\nHEAD -> %d\nTAIL -> %d\n", queue->head, queue->tail);
 }
 
-void printList(int *queue, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d -> ", queue[i]);
-    }
-    printf("NULL\n");
+void clearQueue(struct Queue *queue) {
+    free(queue->values);
+    free(queue);
 }
 
 int main(void) {
-    int *queue;
-    int size = 0;
+    struct Queue *queue = malloc(sizeof(struct Queue));
 
-    queue = (int*)malloc(size * sizeof(int));
+    enqueue(05, queue);
+    enqueue(10, queue);
+    enqueue(20, queue);
+    enqueue(30, queue);
+    enqueue(40, queue);
+    enqueue(50, queue);
+    enqueue(60, queue);
 
-    if (queue == NULL) {
-        printf("Memory allocation error\n");
-        return EXIT_FAILURE;
-    }
+    dequeue(queue);
 
-    enqueue(1, &queue, &size);
-    enqueue(2, &queue, &size);
-    enqueue(3, &queue, &size);
+    printQueue(queue);
 
-    printList(queue, size);
+    clearQueue(queue);
 
-    free(queue);
-    queue = NULL;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
