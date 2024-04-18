d70a05f9b56a7f238aad28c786801cd4d544fbf1
Yagoxdll <113265293+Yagoxdll@users.noreply.github.com>
2024-04-18 20:42:19 -0300

    Comments
    Comments were added to the code explaining how the data structure behaves.

--- binary tree.c
+++ binary tree.c
@@ -1,42 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure to represent a node of the tree
+// Structure to represent a node of the binary tree
 struct TreeNode {
     int value;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
-
-// Function to create a new node of the tree
+// Function to create a new node of the binary tree
 struct TreeNode *createNode(int value) {
+    // Allocate memory for the new node
     struct TreeNode *newNode = malloc(sizeof(struct TreeNode));
+    // Check if memory allocation was successful
     if (!newNode) {
         perror("Memory allocation error");
         exit(EXIT_FAILURE);
     }
+    // Initialize the value and left/right pointers of the new node
     newNode->value = value;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
 }
 
-// Function to insert a value into the tree
+// Function to insert a value into the binary tree
 struct TreeNode *insert(struct TreeNode *root, int value) {
+    // If the root is NULL, create a new node with the given value
     if (root == NULL) {
         return createNode(value);
     }
+    // If the value is less than the root's value, insert it into the left subtree
     if (value < root->value) {
         root->left = insert(root->left, value);
-    } else if (value > root->value) {
+    }
+    // If the value is greater than the root's value, insert it into the right subtree
+    else if (value > root->value) {
         root->right = insert(root->right, value);
     }
     return root;
 }
 
-// Function to print the tree in order
+// Function to print the elements of the binary tree in-order
 void printInOrder(struct TreeNode *root) {
+    // If the root is not NULL, recursively print the left subtree, then the root, and then the right subtree
     if (root != NULL) {
         printInOrder(root->left);
         printf("%d -> ", root->value);
@@ -44,8 +51,9 @@ void printInOrder(struct TreeNode *root) {
     }
 }
 
-// Function to free the memory allocated by the tree
+// Function to free the memory allocated by the binary tree
 void freeTree(struct TreeNode *root) {
+    // If the root is not NULL, recursively free the memory of the left and right subtrees, then free the root node
     if (root != NULL) {
         freeTree(root->left);
         freeTree(root->right);
@@ -56,7 +64,7 @@ void freeTree(struct TreeNode *root) {
 int main() {
     struct TreeNode *tree = NULL;
 
-    // Insertion of values into the tree
+    // Insertion of values into the binary tree
     tree = insert(tree, 0);
     insert(tree, 10);
     insert(tree, 45);
@@ -64,11 +72,11 @@ int main() {
     insert(tree, -25);
     insert(tree, 33);
 
-    // Printing the tree
+    // Printing the binary tree in-order
     printInOrder(tree);
     printf("NULL\n");
 
-    // Freeing memory
+    // Freeing memory allocated by the binary tree
     freeTree(tree);
 
     return 0;
